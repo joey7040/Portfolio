6ef07f2369ca3a0cf6f60288506ae04fb5e0819a
Joey Rivera <jrivera@trellance.com>
2020-02-06 15:25:12 -0500

    new build

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
 import Default from './components/Default404/Default';
 import Mission from './components/Mission/Mission';
-
-import Affiliates from './components/Affiliates/Affiliates';
 import ProjectUC from './components/Projects/ProjectUC';
 import Contact from './components/ContactForm/Contacts';
-import PhotoGallery from './components/Gallery/Gallery';
+
 
 class App extends Component {
   render() {
@@ -20,8 +18,6 @@ class App extends Component {
             <Route exact path="/" component={Mission} />
             <Route path="/mission" component={Mission} />
             <Route path="/projects" component={ProjectUC} />
-            <Route path="/affiliates" component={Affiliates} />
-            {/* <Route path="/gallery" component={PhotoGallery} /> */}
             <Route path="/contact" component={Contact} />
             <Route component={Default} />
           </Switch>
