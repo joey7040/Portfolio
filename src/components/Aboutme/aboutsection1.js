f1f2abca4c090309e501c997c218b1936f680de0
Joey Rivera <jrivera@trellance.com>
2020-06-24 12:49:45 -0400

    update

--- src/components/Aboutme/aboutsection1.js
+++ src/components/Aboutme/aboutsection1.js
@@ -6,7 +6,7 @@ const Section1 = () =>{
     return <div className="container-fluid ab-1">
             <div className="container ">
                 <div className='row'>
-                    <div className='col'>
+                    <div className='col-md-3'>
                         <h2 className="ab-title-1">About me</h2>
                         <div className="ab-text-1">
                             <h5>I'm a software engineer based in sunny Tampa, Florida.</h5>
@@ -18,7 +18,7 @@ const Section1 = () =>{
 
                         </div>
                     </div>
-                    <div className='col'>
+                    <div className='col-md-3'>
                     <img
                             src={require('../../assets/img/meandaidan.jpg')}
                             alt="gamer-pic"
